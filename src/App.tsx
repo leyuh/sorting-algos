d973cf2b6c8890a3cd935ed5f745e238443544bb
Claire Devlin <clairebeargoldielox@icloud.com>
2023-01-07 18:05:58 -0800

    initialize, make bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const visWidth: number = 600;
+  const visHeight: number = 400;
+
+  const marginSize: number = 5;
+
+  const arrLength: number = 25;
+
+  const [arr, setArr] = useState<number[]>([]);
+
+  useEffect(() => {
+    for (let i = 0; i < arrLength; i++) {
+      let randNum: number = Math.ceil(Math.random() * arrLength);
+      setArr((prev) => {
+        return [...prev, randNum];
+      })
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div id="visualizer">
+        {arr.map((val: number, i: number) => {
+          return <span className="bar" id={`bar-${i}`} style={{
+            "height": `${val * ((visHeight - 10) / arrLength)}px`,
+            "width": `${(visWidth / arrLength) - marginSize}px`,
+            "margin": `${marginSize}px`
+          }} key={i}/>
+        })}
+      </div>
     </div>
   );
 }
